a9d11f292e673c97f6002818400d481d44a4c887
Юлиан Колабин <kolabin2015@mail.ru>
2025-12-09 19:03:43 +0300

    update

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,12 +1,12 @@
 from playwright.sync_api import Page, expect
 from pages.base_page import BasePage
 
-class RegistrationPage (BasePage):
+class DashboardPage (BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
         self.page_title = page.locator('//h6[@data-testid="dashboard-toolbar-title-text"]')
     
-    def check_visible_dashboard_page_title(self):
+    def check_visible_title(self):
         expect(self.page_title).to_be_visible()
         expect(self.page_title).to_have_text('Dashboard')
