85082abdb830c5ce3ba3a0e5bffc1ec640f85b8e
Julian <kolabin2015@mail.ru>
2025-12-17 02:32:45 +0300

    Updated components with Page Factory

--- components/courses/course_view_menu_component.py
+++ components/courses/course_view_menu_component.py
@@ -1,22 +1,23 @@
 from components.base_component import BaseComponent
 from playwright.sync_api import Page, expect
+from elements.button import Button
 
 class CourseViewMenuComponent(BaseComponent):
     def __init__(self, page: Page): 
         super().__init__(page)
-
-        self.dropdown_edit_button = page.locator('//li[@data-testid="course-view-edit-menu-item"]')
-        self.dropdown_delete_button = page.locator('//li[@data-testid="course-view-delete-menu-item"]')
-        self.dropdown_open_menu_button = page.locator('//button[@data-testid="course-view-menu-button"]')
-
-    def click_edit_button(self, index):
-        self.dropdown_open_menu_button.nth(index).click()
-
-        expect(self.dropdown_edit_button.nth(index)).to_be_visible()
-        self.dropdown_edit_button.nth(index).click()
-
-    def click_delete_button(self, index):
-        self.dropdown_open_menu_button.nth(index).click()
         
-        expect(self.dropdown_delete_button.nth(index)).to_be_visible()
-        self.dropdown_delete_button.nth(index).click()
+        self.dropdown_edit_button = Button(page, '//li[@data-testid="course-view-edit-menu-item"]', 'Edit')
+        self.dropdown_delete_button = Button(page, '//li[@data-testid="course-view-delete-menu-item"]', 'Delete')
+        self.dropdown_open_menu_button = Button(page, '//button[@data-testid="course-view-menu-button"]', 'Menu')
+
+    def click_edit_button(self, index: int):
+        self.dropdown_open_menu_button.click(nth=index)
+
+        self.dropdown_edit_button.check_visible(nth=index)
+        self.dropdown_edit_button.click(nth=index)
+
+    def click_delete_button(self, index: int):
+        self.dropdown_open_menu_button.click(nth=index)
+        
+        self.dropdown_delete_button.check_visible(nth=index)
+        self.dropdown_delete_button.click(nth=index)
--- components/courses/courses_list_toolbar_view_component.py
+++ components/courses/courses_list_toolbar_view_component.py
@@ -1,19 +1,21 @@
 from components.base_component import BaseComponent
 from playwright.sync_api import Page, expect
+from elements.text import Text
+from elements.button import Button
 import re
 
 class CoursesListToolbarViewComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.title_text = page.locator('//h6[@data-testid="courses-list-toolbar-title-text"]')
-        self.add_course_button = page.locator('//button[@data-testid="courses-list-toolbar-create-course-button"]')
+        self.title_text = Text(page, '//h6[@data-testid="courses-list-toolbar-title-text"]', 'Toolbar title')
+        self.add_course_button = Button(page, '//button[@data-testid="courses-list-toolbar-create-course-button"]', 'Create course button')
     
     def check_visible(self):
-        expect(self.title_text).to_be_visible()
-        expect(self.title_text).to_have_text('Courses')
+        self.title_text.check_visible()
+        self.title_text.check_have_text(text='Courses')
 
-        expect(self.add_course_button).to_be_visible()
+        self.add_course_button.check_visible()
 
     def click_create_course_button(self):
         self.add_course_button.click()
